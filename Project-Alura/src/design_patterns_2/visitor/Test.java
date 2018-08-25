9ce2bd15969865b37b8d3c9fa7011c8389f1df57
Andre Tibola <andre.spironello12@gmail.com>
2018-08-24 21:57:42 -0300

    new description to SOLID

--- Project-Alura/src/design_patterns_2/visitor/Test.java
+++ Project-Alura/src/design_patterns_2/visitor/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         Expression e1 = new Addition(new Number(3), new Number(4));
         Expression e2 = new Division(new Number(10), new Number(5));
-        Expression expression = new Multiplication(new SquareRoot(new Number(25)), new Subtraction(e1, e2));
+        Expression expression = new Multiplication(new SquareRoot(new Number(25)), new Subtraction(new Number(12), new Number(5)));
 
         Visitor visitor = new Visitor();
         expression.accept(visitor);
